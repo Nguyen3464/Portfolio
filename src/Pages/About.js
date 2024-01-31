31e544cc637cd5bd00bd088b1311b8c8670de7ef
Nguyen Nguyen <n.nguyen3464@gmail.com>
2024-01-30 20:01:20 -0600

    working on about page

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,13 +1,57 @@
-import React from 'react'
+import React from "react";
+import certjs from "../Assets/certjs.jpeg"
+import certhtml from "../Assets/certhtml.jpeg"
+import certcss from "../Assets/certcss.jpeg"
+import aam from "../Assets/aam.jpeg"
+import aamcert from "../Assets/aamcert.jpeg"
+import ntc from "../Assets/ntc.jpeg"
+import loc1 from "../Assets/loc1.jpeg"
+import loc2 from "../Assets/loc2.jpeg"
 
 export const About = () => {
   return (
     <>
-      <div className='about-section1'>Introduction</div>
-      <div className='about-section2'>King</div>
-      <div className='about-section3'>Warrior</div>
-      <div className='about-section4'>Magician</div>
-      <div className='about-section5'>Lover</div>
+      <div className="certs-wrapper">
+        <div className="certs">
+          <h1>LEARN Academy</h1>
+        </div>
+        <div className="certs">
+          <img src={certjs} alt=" Javascript Certification" />
+        </div>
+        <div className="certs">
+          <img src={certhtml} alt=" Html Certification" height="300"/>
+        </div>
+        <div className="certs">
+          <img src={certcss} alt=" CSS Certification" height="300"/>
+        </div>
+      </div>
+
+      <div className="awards-wrapper">
+        <div className="awards">
+          <img src={aam} alt="Army Achievement Award" height="300"/>
+        </div>
+        <div className="awards">
+          <img src={aamcert} alt="Army Achievement Award Document" height="300"/>
+        </div>
+        <div className="awards">
+          <img src={ntc} alt="Chief Gram's award" height="300"/>
+        </div>
+      </div>
+
+      <div className="loc-wrapper">
+        <div className="loc">
+          <img src={loc1} alt="Chief Gram's award" height="300"/>
+        </div>
+        <div className="loc">
+          <img src={loc2} alt="Chief Gram's award" height="300"/>
+        </div>
+      </div>
+
+      <div className="about-section1">Introduction</div>
+      <div className="about-section2">King</div>
+      <div className="about-section3">Warrior</div>
+      <div className="about-section4">Magician</div>
+      <div className="about-section5">Lover</div>
     </>
-  )
-}
+  );
+};
--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -1,9 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import logo from '../Assets/logo.jpeg'
-import mushroom1 from '../Assets/mushroom1.webp'
-import mushroom2 from '../Assets/mushroom2.jpeg'
-import mushroom3 from '../Assets/mushroom3.jpeg'
-
+import React, { useEffect, useState } from "react";
+import logo from "../Assets/logo.jpeg";
+import mushroom1 from "../Assets/mushroom1.webp";
+import mushroom2 from "../Assets/mushroom2.jpeg";
+import mushroom3 from "../Assets/mushroom3.jpeg";
 
 export const Projects = () => {
   const [showMotionDiv, setShowMotionDiv] = useState(false);
@@ -16,68 +15,108 @@ export const Projects = () => {
   return (
     <>
       <div>
-        <div className='projects-section1'>
-          <div className='projects-section1-container1'>
+        <div className="projects-section1">
+          <div className="projects-section1-container1">
             <h1>Mushroom Logs</h1>
             <h3>
-              Embark on a fascinating journey into the world of fungi with our exclusive Mushroom Log Workshop.
+              Embark on a fascinating journey into the world of fungi with our
+              exclusive Mushroom Log Workshop.
             </h3>
           </div>
-          <div className={`projects-section1-container2 ${showMotionDiv ? 'activate-motion' : ''}`}>
-            <div className='sub-container1'>
+          <div
+            className={`projects-section1-container2 ${
+              showMotionDiv ? "activate-motion" : ""
+            }`}
+          >
+            <div className="sub-container1">
               <h5>Hands-On Experience</h5>
-              <p>Roll up your sleeves and dive into the enchanting world of mushroom cultivation. Our expert facilitators will provide step-by-step guidance, ensuring you leave with the skills to grow your own mushrooms at home.</p>
+              <p>
+                Roll up your sleeves and dive into the enchanting world of
+                mushroom cultivation. Our expert facilitators will provide
+                step-by-step guidance, ensuring you leave with the skills to
+                grow your own mushrooms at home.
+              </p>
             </div>
-            <div className='sub-container2'>
+            <div className="sub-container2">
               <h5>Interactive Learning</h5>
-              <p>Engage in lively discussions about the different types of mushrooms, the environmental benefits of cultivation, and the sustainable practices involved.</p>
+              <p>
+                Engage in lively discussions about the different types of
+                mushrooms, the environmental benefits of cultivation, and the
+                sustainable practices involved.
+              </p>
             </div>
-            <div className='sub-container3'>
+            <div className="sub-container3">
               <h5>Take Home Your Creation</h5>
-              <p>Take home your inoculated mushroom log, ready to sprout a bountiful harvest of delectable mushrooms in the comfort of your own space</p>
-            </div>  
+              <p>
+                Take home your inoculated mushroom log, ready to sprout a
+                bountiful harvest of delectable mushrooms in the comfort of your
+                own space
+              </p>
+            </div>
           </div>
         </div>
-        <div className='projects-section1-container3'>
-        <img src={logo} alt="logo" className="logo1" />
-            <h1>What to Expect</h1>
-            <h4>Unlock the secrets of cultivating your own gourmet mushrooms at our hands-on workshop. Learn the art and science of mushroom cultivation as we guide you through the process of inoculating logs with spores. Whether you're a gardening enthusiast, a nature lover, or just curious about the mysteries of mycology, this workshop is designed for all levels of experience.</h4>
-          </div>
-        <div className='projects1-section3'>
+        <div className="projects-section1-container3">
+          <img src={logo} alt="logo" className="logo1" />
+          <h1>What to Expect</h1>
+          <h4>
+            Unlock the secrets of cultivating your own gourmet mushrooms at our
+            hands-on workshop. Learn the art and science of mushroom cultivation
+            as we guide you through the process of inoculating logs with spores.
+            Whether you're a gardening enthusiast, a nature lover, or just
+            curious about the mysteries of mycology, this workshop is designed
+            for all levels of experience.
+          </h4>
+        </div>
+        <div className="projects1-section3">
           <h1>Our Process</h1>
-          <div className='projects1-section3-container'>
-            <div className='image-card1'>
-              <div className='image1'>
-                <img src={mushroom1} className='mushroom-images1'/>
+          <div className="projects1-section3-container">
+            <div className="image-card1">
+              <div className="image1">
+                <img src={mushroom1} className="mushroom-images1" />
               </div>
-              <div className='image-text1'>
+              <div className="image-text1">
                 <h4>Preparation</h4>
-                <p>Prepare your mushroom logs by selecting a suitable hardwood, such as oak or maple. Cut the logs to the desired length, and then sterilize them to create a clean environment for the mushroom mycelium to thrive.</p>
+                <p>
+                  Prepare your mushroom logs by selecting a suitable hardwood,
+                  such as oak or maple. Cut the logs to the desired length, and
+                  then sterilize them to create a clean environment for the
+                  mushroom mycelium to thrive.
+                </p>
               </div>
             </div>
-            <div className='image-card2'>
-              <div className='image2'>
-                <img src={mushroom2} className='mushroom-images2'/>
+            <div className="image-card2">
+              <div className="image2">
+                <img src={mushroom2} className="mushroom-images2" />
               </div>
-              <div className='image-text2'>
+              <div className="image-text2">
                 <h4>Inoculation</h4>
-                <p>During the workshop, learn the art of inoculation as you introduce mushroom spores or mycelium to the prepared logs. Witness the fascinating process of mycelial growth, the first step towards cultivating a variety of flavorful and nutritious mushrooms.</p>
+                <p>
+                  During the workshop, learn the art of inoculation as you
+                  introduce mushroom spores or mycelium to the prepared logs.
+                  Witness the fascinating process of mycelial growth, the first
+                  step towards cultivating a variety of flavorful and nutritious
+                  mushrooms.
+                </p>
               </div>
-            
             </div>
-            <div className='image-card1'>
-              <div className='image3'>
-                <img src={mushroom3} className='mushroom-images3'/>
+            <div className="image-card1">
+              <div className="image3">
+                <img src={mushroom3} className="mushroom-images3" />
               </div>
-              <div className='image-text3'>
+              <div className="image-text3">
                 <h4>Preservation</h4>
-                <p>Explore proper storage techniques to maintain the health and vitality of your inoculated logs. Understand the ideal conditions, including temperature and humidity, to ensure a successful mushroom cultivation journey from inoculation to the harvest of your home-grown delights.</p>
+                <p>
+                  Explore proper storage techniques to maintain the health and
+                  vitality of your inoculated logs. Understand the ideal
+                  conditions, including temperature and humidity, to ensure a
+                  successful mushroom cultivation journey from inoculation to
+                  the harvest of your home-grown delights.
+                </p>
               </div>
-
             </div>
           </div>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
